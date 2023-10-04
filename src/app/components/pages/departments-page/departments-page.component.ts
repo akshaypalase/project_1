9a0e6361f765f3d6f5f5a23cc12dc5e47c694d6b
akshay palase <akshaypalase6@gmail.com>
2023-10-05 01:33:35 +0530

    initial commit

---
+++ src/app/components/pages/departments-page/departments-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-departments-page',
+  templateUrl: './departments-page.component.html',
+  styleUrls: ['./departments-page.component.scss']
+})
+export class DepartmentsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
